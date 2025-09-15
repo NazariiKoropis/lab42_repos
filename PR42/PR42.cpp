e20afa4ed3c69db7a9aa67939c038db274406a2d
NazariiKoropis”git config --global user.name NazariiKoropis” <nazarkoropisprogramming@gmail.comgit config --global user.email nazarkoropisprogramming@gmail.com>
2025-09-15 17:12:03 +0300

    fix

--- PR42/PR42.cpp
+++ PR42/PR42.cpp
@@ -5,9 +5,12 @@
 int main() {
     double x_start, x_end, x_step;
 
-    std::cout << "X_start = "; std::cin >> x_start;
-    std::cout << "x_end = "; std::cin >> x_end;
-    std::cout << "x_step = "; std::cin >> x_step;
+    std::cout << "X_start = "; 
+    std::cin >> x_start;
+    std::cout << "x_end = ";
+    std::cin >> x_end;
+    std::cout << "x_step = ";
+    std::cin >> x_step;
 
     std::cout << std::fixed;
     std::cout << "---------------------------" << std::endl;
@@ -21,11 +24,12 @@ int main() {
             y = 2 * x - 13.5 - (std::sin(x)) / (1 + std::pow(std::cos(x), 2));
         }
         else if (x <= 1) {
-            y = 2 * x - 3.5 * std::pow(std::cos(std::sin(x)), 2) - 1;
+            y = 2 * x - 13.5 - (std::pow(std::cos(x), 2) * std::pow(std::sin(x), 2) * x - 1);
         }
         else {
-            y = 2 * x - 13.5 * std::log(x + 0.4);
+            y = 2 * x - 13.5 - std::log10(x + 0.4);
         }
+
 
         std::cout << "|" << std::setw(7) << std::setprecision(2) << x
             << "   |" << std::setw(10) << std::setprecision(3) << y
