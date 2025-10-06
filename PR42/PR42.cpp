553f221265bf94e082f58b206a7b073378f28afa
NazariiKoropis”git config --global user.name NazariiKoropis” <nazarkoropisprogramming@gmail.comgit config --global user.email nazarkoropisprogramming@gmail.com>
2025-10-06 19:11:32 +0300

    main2

--- PR42/PR42.cpp
+++ PR42/PR42.cpp
@@ -1,41 +1,42 @@
 ﻿#include <iostream>
 #include <iomanip>
 #include <cmath>
+using namespace std;
 
 int main() {
     double x_start, x_end, x_step;
 
-    std::cout << "X_start = "; 
-    std::cin >> x_start;
-    std::cout << "x_end = ";
-    std::cin >> x_end;
-    std::cout << "x_step = ";
-    std::cin >> x_step;
+    cout << "X_start = ";
+    cin >> x_start;
+    cout << "X_end = ";
+    cin >> x_end;
+    cout << "X_step = ";
+    cin >> x_step;
 
-    std::cout << std::fixed;
-    std::cout << "---------------------------" << std::endl;
-    std::cout << "|" << std::setw(5) << "x" << "     |"
-        << std::setw(7) << "y" << "       |" << std::endl;
-    std::cout << "---------------------------" << std::endl;
+    cout << fixed;
+    cout << "----------------------------------------" << endl;
+    cout << "|" << setw(8) << "x" << " |"
+        << setw(15) << "y" << " |" << endl;
+    cout << "----------------------------------------" << endl;
 
-    for (double x = x_start; x < x_end; x += x_step) {
+    for (double x = x_start; x <= x_end; x += x_step) {
         double y;
+
         if (x < -1) {
-            y = 2 * x - 13.5 - (std::sin(x)) / (1 + std::pow(std::cos(x), 2));
+            y = 2 * x - 13.5 - (sin(x)) / (1 + pow(cos(x), 2));
         }
         else if (x <= 1) {
-            y = 2 * x - 13.5 - (std::pow(std::cos(x), 2) * std::pow(std::sin(x), 2) * x - 1);
+            y = 2 * x - 13.5 - (pow(cos(sin(x)), 2) - 1);
         }
         else {
-            y = 2 * x - 13.5 - std::log10(x + 0.4);
+            y = 2 * x - 13.5 - log10(x + 0.4);
         }
 
-
-        std::cout << "|" << std::setw(7) << std::setprecision(2) << x
-            << "   |" << std::setw(10) << std::setprecision(3) << y
-            << "    |" << std::endl;
+        cout << "|" << setw(8) << setprecision(2) << x
+            << " |" << setw(15) << setprecision(5) << y
+            << " |" << endl;
     }
-    std::cout << "---------------------------" << std::endl;
 
+    cout << "----------------------------------------" << endl;
     return 0;
 }
